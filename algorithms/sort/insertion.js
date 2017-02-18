822df395557d8a2863e3d1a527ad966da19a162e
Albin George <albinin0002@gmail.com>
2017-02-18 16:49:47 +0530

    Added insertion sort

--- algorithms/sort/insertion.js
+++ algorithms/sort/insertion.js
@@ -1,3 +1,25 @@
 module.exports = function() {
+
+    var input = [3, 6, 1, 7, 2, 3, 9, 8];
+    let print_input = () => {
+        console.log(input.join(", "));
+    }
     console.log("Welcome to insertion sort!");
+    console.log("Input:");
+    print_input();
+
+    for(let i=1; i<input.length; i++) {
+        for(let j=i - 1; j>=0; j--) {
+            if(input[j + 1] < input[j]) {
+                print_input();
+                let temp = input[j + 1];
+                input[j + 1] = input[j];
+                input[j] = temp;
+            } else {
+                break;
+            }
+        }
+    }
+    console.log("Output:");
+    print_input();
 }
